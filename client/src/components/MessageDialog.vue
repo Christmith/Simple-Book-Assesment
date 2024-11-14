f87689b8527bb78cf0c6e6cdd4172aef2aa9102a
Yohan Christmith Weerapperuma <it22328434@my.sliit.lk>
2024-11-15 00:02:44 +0530

    Add MessageDialog component for displaying messages with different types

---
+++ client/src/components/MessageDialog.vue
@@ -0,0 +1,57 @@
+<!-- MessageDialog.vue -->
+<template>
+  <div
+    v-if="visible"
+    class="fixed inset-0 bg-gray-500 bg-opacity-50 flex justify-center items-center"
+  >
+    <div class="bg-white rounded-lg p-6 shadow-lg max-w-sm w-full" @click.stop>
+      <h3
+        :class="{
+          'text-green-600': messageType === 'success',
+          'text-red-600': messageType === 'error',
+        }"
+        class="text-xl font-bold mb-4"
+      >
+        {{ messageTitle }}
+      </h3>
+      <p class="mb-6">{{ messageText }}</p>
+      <button
+        @click="closeDialog"
+        class="bg-purple-500 text-white py-2 px-6 rounded-md hover:bg-purple-600 transition duration-300"
+      >
+        OK!
+      </button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MessageDialog",
+  props: {
+    visible: {
+      type: Boolean,
+      required: true,
+    },
+    messageType: {
+      type: String,
+      required: true,
+    },
+    messageTitle: {
+      type: String,
+      required: true,
+    },
+    messageText: {
+      type: String,
+      required: true,
+    },
+  },
+  methods: {
+    closeDialog() {
+      this.$emit("close");
+    },
+  },
+};
+</script>
+
+<style scoped></style>
